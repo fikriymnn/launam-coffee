a62b36c29b1a85f4f64980ed0268540c92405911
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-08-10 13:29:25 +0700

    upt

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../lib/firebase/page"
 import { useAuth } from "@/components/authService";
+import Footer from "@/components/Footer";
 
 
 
@@ -41,29 +42,34 @@ export default function Admin() {
   };
   return (
     <>
-      <h1 className="text-6xl font-bold text-center mt-24 mb-24 text-[#3C2A21]">
+      
+      <br/>
+      <div className="bg-[#3C2A21] w-[500px] mx-auto rounded-xl mt-20 mb-20">
+      <h1 className="text-3xl font-bold text-center pt-10 mb-5 text-white">
         Login
       </h1>
-      <div className="bg-[#3C2A21] w-[500px] mx-auto rounded-xl">
-        <div className="p-20">
-          <form onSubmit={onSubmit}>
+        <div className="pb-20 pl-20 pr-20">
+          <form onSubmit={onSubmit} className="">
             <div>
-              <label for="email">email :</label>
+              <p >Email :</p>
               <input
                 id="email"
                 name="email"
                 type="text"
-                className="text-black my-5"
+                placeholder="Masukan Email"
+                className="text-black my-5 w-80"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
+            
             <div>
-              <label for="password">Password :</label>
+              <p>Password :</p>
               <input
                 id="password"
                 name="password"
                 type="password"
-                className="text-black my-5"
+                placeholder="Masukan Password"
+                className="text-black my-5  w-80"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
@@ -75,6 +81,7 @@ export default function Admin() {
           </form>
         </div>
       </div>
+      <Footer/>
     </>
   );
 }
