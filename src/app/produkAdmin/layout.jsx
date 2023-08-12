d22934f095d7ebae5b3ab30d010d9e26087a3911
acepW <acepwahyu001@gmail.com>
2023-08-12 17:45:25 +0700

    updatee

--- src/app/produkAdmin/layout.jsx
+++ src/app/produkAdmin/layout.jsx
@@ -4,17 +4,17 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { redirect } from 'next/navigation'
 
 export default function Layout({
-    children
-  }) {
-    const check = typeof window !== 'undefined' ? localStorage.getItem('auth') : null
+  children
+}) {
+  const check = typeof window !== 'undefined' ? localStorage.getItem('auth') : null
 
-    const { push } = useRouter();
-     const login = true;
-    
-     
-    return (
-      <>
-      {check=="22gg12js"?children:redirect("/")}
-      </>
-    )
-  }
+  const { push } = useRouter();
+  const login = true;
+
+
+  return (
+    <>
+      {check == "22gg12js" ? children : redirect("/")}
+    </>
+  )
+}
